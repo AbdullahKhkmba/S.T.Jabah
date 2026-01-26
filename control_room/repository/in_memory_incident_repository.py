b061a5221ba69bf046ce6420ad82545e800acecc
Abdelrhman Shaker <abd.0sh911@gmail.com>
2026-01-26 13:16:09 +0200

    list all incidents v2

--- control_room/repository/in_memory_incident_repository.py
+++ control_room/repository/in_memory_incident_repository.py
@@ -20,7 +20,8 @@ class InMemoryIncidentRepository(IncidentRepository):
         Returns:
             Created entity with ID
         """
-        pass
+        self._storage[entity.id] = entity
+        return entity
 
     def get_by_id(self, entity_id: str) -> Optional[Incident]:
         """
@@ -65,4 +66,4 @@ class InMemoryIncidentRepository(IncidentRepository):
         Returns:
             List of all entities
         """
-        pass
+        return list(self._storage.values())
